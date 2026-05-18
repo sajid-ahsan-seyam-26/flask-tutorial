75a6dd06bad431f585d5fe87eca6d58fa9e3d6fc
sajid-ahsan-seyam-26 <sajidahsanseyamstuden>
2026-05-18 21:51:28 +0600

    upload some part

--- app3.py
+++ app3.py
@@ -11,20 +11,16 @@ def login():
     username = request.form.get("username")
     password = request.form.get("password")
 
-    """if username == "seyam" and password == "123":
+    valid_users = {
+        "admin": "123",
+        "sagar": "pass",
+        "rajat": "raj"
+    }
+
+    if username in valid_users and password == valid_users[username]:
         return render_template("home.html", name=username)
     else:
-        return "invalid thik kore koro seyam"""
-    valid_users={
-        "admin":"123",
-        "sagar":"pass",
-        "rajat":"raj"
-
-    }
-    if username in valid_users and password==valid_users[username]:
-        return render_template("homme.html",name=username)
-    else:
-        return "invalid credentails"
+        return "invalid credentials"
 
 if __name__ == "__main__":
     app.run(debug=True)
